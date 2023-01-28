61837e23ee8b67b81819fc826737c6e2e6cb5f64
VIGNESH-TRL769 <123439677+VIGNESH-TRL769@users.noreply.github.com>
2023-01-28 18:43:42 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,14 @@
 package org.example;
 
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+import java.util.logging.Logger;
 class Student
 {
-    private static final Logger LOGGER =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    float gpa;
+    public static final Logger LOGGER =  Logger.getLogger("InfoLogging");
+    public  float gpa;
     public final String name;
-    String  grade;
+    public String  grade;
     Student(String name,float gpa,String  grade)
     {
         this.name=name;
@@ -48,7 +47,7 @@ class Student
            grade="F";
 
         }
-        LOGGER.log(Level.INFO,"---Updated Successfully---");
+        LOGGER.info("---Updated Successfully---");
 
     }
     public String display()
@@ -59,16 +58,16 @@ class Student
 }
 public class Main
 {
-    private static final Logger LOGGER =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    public static final Logger LOGGER =  Logger.getLogger("InfoLogging");
     public static void main(String[] args)
     {
         Scanner input=new Scanner(System.in);
         float gpa;
-        LOGGER.log(Level.INFO,"Enter the Student Name");
+        LOGGER.info("Enter the Student Name:");
         String name=input.nextLine();
         while(true)
         {
-            LOGGER.log(Level.INFO, "Enter the GPA:");
+            LOGGER.info( "Enter the GPA:");
             gpa = input.nextFloat();
             if(gpa>0 && gpa<=10)
             {
@@ -76,36 +75,32 @@ public class Main
             }
             else
             {
-                LOGGER.log(Level.INFO,"Please enter Valid GPA");
+                LOGGER.info("Please enter Valid GPA");
             }
 
         }
-        LOGGER.log(Level.INFO,"Enter the gradeLevel:");
+        LOGGER.info("Enter the gradeLevel:");
         String grade=input.next();
         Student student=new Student(name,gpa, grade);
         int temp=0;
         do {
 
-            LOGGER.log(Level.INFO,"\n1.Update\n2.CheckData\n3.Exit\nEnter the Choice:");
+            LOGGER.info("\n1.Update\n2.CheckData\n3.Exit\nEnter the Choice:");
             int key = input.nextInt();
             switch (key) {
                 case 1 -> {
-                    LOGGER.log(Level.INFO,"Enter the GPA:");
+                    LOGGER.info("Enter the GPA:");
                     float value = input.nextFloat();
                     if(value>0 && value<=10)
                         student.update(value);
                     else
-                        LOGGER.log(Level.INFO,"please enter valid GPA");
+                        LOGGER.info("please enter valid GPA");
 
                 }
-                case 2 ->  {String content=student.display();LOGGER.log(Level.INFO,content);}
-                default -> {temp = 1; LOGGER.log(Level.INFO,"---Thank You---");}
+                case 2 ->  {String content=student.display();LOGGER.info(content);}
+                default -> {temp = 1; LOGGER.info("---Thank You---");}
             }
 
         } while (temp != 1);
-
-
-
-
     }
 }
