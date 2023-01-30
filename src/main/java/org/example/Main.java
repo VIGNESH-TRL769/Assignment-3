8d4f6a9e00d8ea7aab4355409c7b29567624adb8
VIGNESH-TRL769 <123439677+VIGNESH-TRL769@users.noreply.github.com>
2023-01-30 17:50:13 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,31 +20,31 @@ class Student
         gpa=value;
         if(value>=9)
         {
-           grade="O";
+            grade="O";
         }
         else if(value>=8)
         {
-           grade="A+";
+            grade="A+";
 
         }
         else if(value>=7)
         {
-           grade="A";
+            grade="A";
 
         }
         else if(value>=6)
         {
-           grade="B+";
+            grade="B+";
 
         }
         else if(value>=5)
         {
-           grade="B";
+            grade="B";
 
         }
         else
         {
-           grade="F";
+            grade="F";
 
         }
         LOGGER.info("---Updated Successfully---");
@@ -56,51 +56,60 @@ class Student
         return name+" has a "+dublicate+" GPA "+" and grade is "+ grade;
     }
 }
-public class Main
-{
-    public static final Logger LOGGER =  Logger.getLogger("InfoLogging");
-    public static void main(String[] args)
-    {
-        Scanner input=new Scanner(System.in);
+public class Main {
+    public static final Logger LOGGER = Logger.getLogger("InfoLogging");
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         float gpa;
         LOGGER.info("Enter the Student Name:");
-        String name=input.nextLine();
-        while(true)
-        {
-            LOGGER.info( "Enter the GPA:");
-            gpa = input.nextFloat();
-            if(gpa>0 && gpa<=10)
-            {
-                break;
-            }
-            else
-            {
-                LOGGER.info("Please enter Valid GPA");
-            }
+        String name = input.nextLine();
+        while (true) {
+
+                LOGGER.info("Enter the GPA:");
+                gpa = input.nextFloat();
+                if (gpa > 0 && gpa <= 10) {
+                    break;
+                } else {
+                    LOGGER.info("Please enter Valid GPA");
+                }
+
 
         }
         LOGGER.info("Enter the gradeLevel:");
-        String grade=input.next();
-        Student student=new Student(name,gpa, grade);
-        int temp=0;
+        String grade = input.next();
+        Student student = new Student(name, gpa, grade);
+        int temp = 0;
         do {
+            try {
 
-            LOGGER.info("\n1.Update\n2.CheckData\n3.Exit\nEnter the Choice:");
-            int key = input.nextInt();
-            switch (key) {
-                case 1 -> {
-                    LOGGER.info("Enter the GPA:");
-                    float value = input.nextFloat();
-                    if(value>0 && value<=10)
-                        student.update(value);
-                    else
-                        LOGGER.info("please enter valid GPA");
+                LOGGER.info("\n1.Update\n2.CheckData\n3.Exit\nEnter the Choice:");
+                int key = input.nextInt();
+                switch (key) {
+                    case 1 -> {
+                        LOGGER.info("Enter the GPA:");
+                        float value = input.nextFloat();
+                        if (value > 0 && value <= 10)
+                            student.update(value);
+                        else
+                            LOGGER.info("please enter valid GPA");
 
+                    }
+                    case 2 -> {
+                        String content = student.display();
+                        LOGGER.info(content);
+                    }
+                    default -> {
+                        temp = 1;
+                        LOGGER.info("---Thank You---");
+                    }
                 }
-                case 2 ->  {String content=student.display();LOGGER.info(content);}
-                default -> {temp = 1; LOGGER.info("---Thank You---");}
-            }
 
+            } catch (Exception e) {
+                String value = "Please Enter Valid Deatils:" + e;
+                LOGGER.info(value);
+                input.nextLine();
+            }
         } while (temp != 1);
     }
 }
